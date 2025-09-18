c61005683af9b495fce858d5a53d483412092248
AnwarAsry <anwareducation03@gmail.com>
2025-09-18 10:55:37 +0200

    init

---
+++ src/main/java/org/sprinto/anwar/demo/Author.java
@@ -0,0 +1,4 @@
+package org.sprinto.anwar.demo;
+
+public class Author {
+}
---
+++ src/main/java/org/sprinto/anwar/demo/Main.java
@@ -0,0 +1,7 @@
+package org.sprinto.anwar.demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
